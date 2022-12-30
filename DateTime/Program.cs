5f5cc3f655fc55b9a7bb6f9b79f9de3f1dd20374
Prajuk Nusbaum <deomax@yahoo.com>
2022-12-30 13:13:35 -0600

    Add project files.

---
+++ DateTime/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.Globalization;
+
+DateTime today= DateTime.Now;
+
+//DateTime birthday = DateTime.Parse(s: "8/22/1969");
+
+// DateTime birthday = DateTime.ParseExact(s: "8/22/1969", format: "d/M/yyyy", CultrueInfo.InvariantCulture);
+
+// DateTime birthday = DateTime.Parse(s:"8/2/1969", 
+
+
+
+Console.WriteLine(today.ToString(format:"MMMM dd, yyyy hh:mm tt zzz"));
